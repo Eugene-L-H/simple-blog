2d2b6a422602a7939a2fda8644dab84c189d9566
Eugene Helland <eugenehelland@mail.com>
2022-06-03 13:27:37 -0600

    Remove helper functions, use ejs in index.ejs insted, move global variable to express_server.js

--- express_server.js
+++ express_server.js
@@ -11,6 +11,33 @@ app.use(morgan('dev'));
 // HELPER FUNCTIONS
 
 // GLOBAL VARIABLES
+const tempBlogsArr = [
+  // Temporary hard code for blog entries. To be replaced with database values
+  {
+    'author': 'Eugene',
+    'title': 'Heartbroken and Frustrated',
+    'content': 'This would be the content for the first blog post.',
+    'date': '2022-05-15'
+  },
+  {
+    'author': 'Eugene',
+    'title': 'Happy Birthday Mom',
+    'content': 'This would be the content for the first blog post. Happy birthday Mom',
+    'date': '2022-03-31'
+  },
+  {
+    'author': 'Eugene',
+    'title': 'Happy B-day Dad',
+    'content': 'This would be the content for the first blog post. Dad, Happy Birthday.',
+    'date': '2022-01-05'
+  },
+  {
+    'author': 'Eugene',
+    'title': 'Merry Christmas!',
+    'content': 'This would be the content for the fourth blog post. Merry Christmas.',
+    'date': '2021-12-25'
+  },
+]
 
 // Listen for incoming requests
 
@@ -21,5 +48,5 @@ app.listen(PORT, () => {
 // GET ROUTES
 
 app.get('*', (req, res) => {
-  res.render('home');
+  res.render('index', { tempBlogsArr });
 })
