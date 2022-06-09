d82ebee6a022c1f35f98d15add5a21edf3647fc3
Eugene Helland <eugenehelland@mail.com>
2022-06-09 16:19:16 -0600

    Write function within edit_blog_post.js that generates the HTML that will be used to edit individual posts.

--- public/scripts/helper_funcitons/edit_blog_post.js
+++ public/scripts/helper_funcitons/edit_blog_post.js
@@ -1,29 +1,43 @@
 const editButtons = document.querySelectorAll('.edit-post');
 const postBodies = document.querySelectorAll('.content');
 
+const createEditPostHTML = (bodyText, postID) => {
+    // Create new div to contain the textarea for editing
+    const editDiv = document.createElement('div');
+    editDiv.setAttribute('name', postID);
+    let editTextarea = document.createElement('textarea');
+    editTextarea.setAttribute('style', 'resize: none');
+    editTextarea.innerHTML = bodyText;
+
+    // Create confirm or cancel buttons
+    let cancelButton = document.createElement('input');
+    cancelButton.setAttribute('class', 'edit-btn');
+    cancelButton.setAttribute('value', '❌');
+    let confirmButton = document.createElement('input');
+    confirmButton.setAttribute('class', 'edit-btn')
+    confirmButton.setAttribute('value', '✅');
+    
+    // Append created elements to editDiv
+    editDiv.append(editTextarea);
+    editDiv.append(cancelButton);
+    editDiv.append(confirmButton);
+
+    return editDiv;
+}
+
+
 for (let button of editButtons) {
   button.addEventListener('click', (e) => {
-
     const postID = e.target["name"];
 
     for (let postBody of postBodies) {
       if (postBody.getAttribute('name') === postID) {
         const bodyText = postBody.innerHTML;
 
-        const editDiv = document.createElement('div');
-        editDiv.setAttribute('name', postID);
-        let editTextarea = document.createElement('textarea');
-        editTextarea.setAttribute('style', 'resize: none');
-        editTextarea.innerHTML = bodyText;
-        editDiv.append(editTextarea);
-        console.log('editTextArea: ', editDiv)
-        // postBody = `
-        //   <textarea style="resize: none">
-        //     ${bodyText}
-        //   </textarea>
-        // `;
+        const editPostHTML = createEditPostHTML(bodyText, postID);
 
-        postBody = editTextarea;
+        console.log('editPostHTML: ', editPostHTML)
+
       }
     }
   });
