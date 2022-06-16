1a0a34351e146e230fc3395b7b6eff9435ff5553
Eugene Helland <eugenehelland@mail.com>
2022-06-16 12:52:00 -0600

    Add borders to the blog area and header, some other general styling.

--- public/scripts/submit_blog_post.js
+++ public/scripts/submit_blog_post.js
@@ -1,14 +1,21 @@
 const blogEntries = document.querySelector('.blog-entries');
-const submitButton = document.querySelector('.submit-post');
 const title = document.querySelector('.post-title');
 const postBody = document.querySelector('.post-body');
+const submitButton = document.querySelector('.submit-post');
+const cancelButton = document.querySelector('.cancel-btn');
 
 submitButton.addEventListener('click', () => {
   const post_title = title.value;
   const post_body = postBody.value;
 
+  if (post_title === '' || post_body === '') {
+    alert('New post requires at title and content.');
+    return;
+  }
+
   // prepare http request to be sent to the server
   let xhr = new XMLHttpRequest();
+
   xhr.open('POST', `/new-post/${post_title}`);
 
     if (confirm('Submit post?')) {
@@ -19,4 +26,9 @@ submitButton.addEventListener('click', () => {
     }
     // Reload the window to trigger repopulation of blog posts
     window.location.reload(true);
+});
+
+cancelButton.addEventListener('click', () => {
+  newPostForm.classList.toggle('hidden');
+  newPostButton.classList.toggle('hidden');
 });
