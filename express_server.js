16858e244448772f2abfef2aeff745d3f8c67ef9
Eugene Helland <eugenehelland@mail.com>
2022-06-18 09:53:31 -0600

    Fix issue with login where error message being displayed when trying to login even with correct credentials.

--- express_server.js
+++ express_server.js
@@ -3,38 +3,37 @@ const res = require("express/lib/response"); // do I need this?
 const app = express();
 
 const PORT = 8080;
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // MIDDLEWARE
-const morgan = require('morgan');
-app.use(morgan('dev'));
+const morgan = require("morgan");
+app.use(morgan("dev"));
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.text({ type: "text/plain" }));
 
 // Password encryption
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 // Use static module so client-side js can be linked to ejs file
 const path = require("path");
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 // GLOBAL VARIABLES
 let loggedIn = false;
-let currentUser = ''; // user who is currently logged in
+let currentUser = ""; // user who is currently logged in
 
 // CONNECT TO DATABASE
-const { Client } = require('pg');
+const { Client } = require("pg");
 const { name } = require("ejs");
 
 const client = new Client({
-  host: 'localhost',
-  user: 'labber',
-  password: 'labber',
-  database: 'simpleblog',
-  port: 5432
-})
+  host: "localhost",
+  user: "labber",
+  password: "labber",
+  database: "simpleblog",
+  port: 5432,
+});
 
 client.connect();
 
@@ -42,26 +41,29 @@ let blogPosts;
 
 // Pull blog posts from database submitted by current user
 const retrieveBlogPostsFromDatabase = () => {
-  client.query(`SELECT * FROM blog_posts WHERE author = '${currentUser}'`, (err, res) => {
-    if(!err) {
-      console.log('Fetching posts from database.')
-      blogPosts = res.rows;
-      blogPosts = blogPosts.sort((a, b) => {
-        return a.post_date - b.post_date;
-      });
-    } else {
-      console.log('Error retrieving posts: ', err.message);
+  client.query(
+    `SELECT * FROM blog_posts WHERE author = '${currentUser}'`,
+    (err, res) => {
+      if (!err) {
+        console.log("Fetching posts from database.");
+        blogPosts = res.rows;
+        blogPosts = blogPosts.sort((a, b) => {
+          return a.post_date - b.post_date;
+        });
+      } else {
+        console.log("Error retrieving posts: ", err.message);
+      }
+      // client.end; - don't think I need this..
     }
-    // client.end; - don't think I need this..
-  });
-}
+  );
+};
 
 retrieveBlogPostsFromDatabase();
 
 // POST ROUTES
 
 // User Login
-app.post('/login/:username', (req, res) => {
+app.post("/login/:username", (req, res) => {
   const username = req.params.username;
   const password = req.body;
 
@@ -71,32 +73,30 @@ app.post('/login/:username', (req, res) => {
   `;
 
   client.query(getPasswordFromDB, (err, queryResults) => {
-    if(!err) {
+    if (!err) {
       // If compareSync with provided password passes credentials are valid
-      if(bcrypt.compareSync(password, queryResults.rows[0]['user_password'])) {
+      if (bcrypt.compareSync(password, queryResults.rows[0]["user_password"])) {
         loggedIn = true;
         currentUser = username;
         console.log(`Logging ${currentUser} in...`);
 
         // Load posts from database authored by logged in user
         retrieveBlogPostsFromDatabase();
-        res.render('index', { blogPosts, currentUser, loggedIn })
+        res.render("index", { blogPosts, currentUser, loggedIn });
       } else {
-        console.log('Username or password does not match.')
-        res.send('Username or password does not match.')
+        console.log('Username or password does not match.');
+        res.sendStatus(404);
       }
     } else {
-      console.log('Error: ', err.message);
+      console.log("Error: ", err.message);
     }
   });
-
 });
 
 // DELETE post
-app.post('/:post_id/delete', (req, res) => {
-
-  console.log('Delete request recieved');
-  const post_id = req.params['post_id'];
+app.post("/:post_id/delete", (req, res) => {
+  console.log("Delete request recieved");
+  const post_id = req.params["post_id"];
 
   const deletePost = `
     DELETE FROM blog_posts
@@ -105,57 +105,64 @@ app.post('/:post_id/delete', (req, res) => {
   `;
 
   client.query(deletePost, (err, res) => {
-    if(!err) {
-      console.log('Deleting post from db');
+    if (!err) {
+      console.log("Deleting post from db");
       // Refresh blogPosts variable with updated database
       retrieveBlogPostsFromDatabase();
     } else {
-      console.log('Error deleting post: ', err.message);
+      console.log("Error deleting post: ", err.message);
     }
   });
 });
 
 // CREATE new blog post
-app.post('/new-post/:post_title', (req, res) => {
-  console.log('recieved incoming post')
-  const postTitle = req.params['post_title'];
+app.post("/new-post/:post_title", (req, res) => {
+  console.log("recieved incoming post");
+  const postTitle = req.params["post_title"];
   const postBody = req.body;
   const todayDate = new Date();
-  postDate = todayDate.toISOString().split('T')[0];
-  
+  postDate = todayDate.toISOString().split("T")[0];
+
   const addPost = `
   INSERT INTO blog_posts (author, title, content, post_date)
   VALUES ('${currentUser}', '${postTitle}', '${postBody}', '${postDate}')
   `;
-  
+
   client.query(addPost, (err, respond) => {
-    if(!err) {
-      console.log('Creating new post');
+    if (!err) {
+      console.log("Creating new post");
       retrieveBlogPostsFromDatabase();
-      return res.render('index', { blogPosts, currentUser, loggedIn });
+      return res.render("index", { blogPosts, currentUser, loggedIn });
     } else {
-      console.log('Error creating post: ', err.message);
+      console.log("Error creating post: ", err.message);
     }
   });
 });
 
 // Register new user
-app.post('/register-user', (req, res) => {
-  console.log('Registration data recieved');
+app.post("/register-user", (req, res) => {
+  console.log("Registration data recieved");
   const registrationQueryString = req.body;
 
   // Convert query-string to JS object extract username + password
-  const nameAndPassword = JSON.parse('{"' + registrationQueryString.replace(/&/g, '","').replace(/=/g,'":"') + '"}', (key, value) => { return key===""?value:decodeURIComponent(value) });
+  const nameAndPassword = JSON.parse(
+    '{"' +
+      registrationQueryString.replace(/&/g, '","').replace(/=/g, '":"') +
+      '"}',
+    (key, value) => {
+      return key === "" ? value : decodeURIComponent(value);
+    }
+  );
 
-  const username = nameAndPassword['username'];
-  const password = nameAndPassword['password'];
+  const username = nameAndPassword["username"];
+  const password = nameAndPassword["password"];
   const hashedPassword = bcrypt.hashSync(password, 10);
 
   // Prevent users with empty username or password from being registered
-  if(username === '' || password === '') {
+  if (username === "" || password === "") {
     return res
       .status(400)
-      .send('Both password and email fields must be filled out to register.');
+      .send("Both password and email fields must be filled out to register.");
   }
 
   // TODO: HASH/ENCRYPT PASSWORD BEFORE SUBMITTING *
@@ -166,51 +173,49 @@ app.post('/register-user', (req, res) => {
   `;
 
   client.query(registerUser, (err, res) => {
-    if(!err) {
+    if (!err) {
       console.log(`${username} registered as a user in the database.`);
       retrieveBlogPostsFromDatabase();
     } else {
-      console.log('Error registering user: ', err.message);
+      console.log("Error registering user: ", err.message);
     }
   });
-
 });
 
 // UPDATE blog post content from user edit
-app.post('/edit-post/:post_ID', (req, res) => {
-  console.log('Sever recieved edited post')
+app.post("/edit-post/:post_ID", (req, res) => {
+  console.log("Sever recieved edited post");
 
   const editedContent = `
     UPDATE blog_posts
     SET content = '${req.body}'
-    WHERE id = ${req.params['post_ID']};
+    WHERE id = ${req.params["post_ID"]};
   `;
 
   client.query(editedContent, (err, res) => {
-    if(!err) {
-      console.log('Blog post content updated');
+    if (!err) {
+      console.log("Blog post content updated");
       retrieveBlogPostsFromDatabase();
     } else {
-      console.log('Error updating content: ', err.message);
+      console.log("Error updating content: ", err.message);
     }
   });
-})
+});
 
 // GET ROUTES
 
 // Log user out
-app.get('/logout', (req, res) => {
+app.get("/logout", (req, res) => {
   loggedIn = false;
-  currentUser = '';
-  
-  return res.render('index', {blogPosts, currentUser, loggedIn});
+  currentUser = "";
+
+  return res.render("index", { blogPosts, currentUser, loggedIn });
 });
 
 // Render main page
-app.get('/', (req, res) => {
-  res.render('index', { blogPosts, currentUser, loggedIn });
+app.get("/", (req, res) => {
+  res.render("index", { blogPosts, currentUser, loggedIn });
 });
-
 
 // Listen for incoming requests
 app.listen(PORT, () => {
--- public/scripts/login.js
+++ public/scripts/login.js
@@ -15,23 +15,27 @@ const addOnClickToLogin = () => {
     const xhr = new XMLHttpRequest();
     xhr.open("POST", `/login/${username}`, false);
     xhr.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        console.log( xhr.responseText );
+      if (this.readyState == 4 && this.status == 404) {
+        // console.log( xhr.responseText );
 
         // Display login error message above login form
         const errorMessage = document.querySelector('.login-error');
         errorMessage.classList.toggle('hidden');
-        return;
+      } else {
+        setTimeout(() => {
+          window.location.reload(true);
+        }, 500);
       }
     };
 
     xhr.send(password);
 
+    const reloadPage = () => {
+      
+    }
     // TODO: Reload page after recieving response from server.
     // A hack-ey workaround to reloading the page after login (await?):
-    // setTimeout(() => {
-    //   window.location.reload(true);
-    // }, 500);
+ 
   });
 };
 
